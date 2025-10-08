e8104de6d763a259b36dfa91a25fe00fcbef7ec0
vitalii-t12 <vitalii.toderian@gmail.com>
2025-10-08 11:05:53 +0300

    fix serve command

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    allowedHosts: ['4577ba38cbf3.ratio1.link']
   },
   preview: {
     host: '0.0.0.0',
-    port: 4173
+    port: 4173,
+    allowedHosts: ['4577ba38cbf3.ratio1.link']
   }
 })
